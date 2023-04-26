e85a3b83448c5ff5bed3d716d9e2987bbd372bf6
Sergejs-Rudko <sergejs.rudko@gmail.com>
2023-04-26 03:32:11 +0100

    feat: dummy version, full key array , duo language support

--- src/index.js
+++ src/index.js
@@ -7,32 +7,543 @@ const keyBoardKeys = document.querySelector('.keyboard__keys');
 
 let virtualKeyboard = {
   shiftDown: false,
+  primaryLanguage: true,
   keySetup: [
+    {
+      value: '`',
+      shiftValue: '~',
+      secondaryValue: 'ё',
+      secondaryShiftValue: 'Ё',
+      code: 'Backquote',
+      isPressed: false,
+    },
     {
       value: '1',
       shiftValue: '!',
+      secondaryValue: '1',
+      secondaryShiftValue: '!',
       code: 'Digit1',
       isPressed: false,
     },
     {
       value: '2',
       shiftValue: '@',
+      secondaryValue: '2',
+      secondaryShiftValue: '"',
       code: 'Digit2',
       isPressed: false,
     },
     {
       value: '3',
       shiftValue: '#',
+      secondaryValue: '3',
+      secondaryShiftValue: '№',
       code: 'Digit3',
+      isPressed: false,
+    },
+    {
+      value: '4',
+      shiftValue: '$',
+      secondaryValue: '4',
+      secondaryShiftValue: ';',
+      code: 'Digit4',
+      isPressed: false,
+    },
+    {
+      value: '5',
+      shiftValue: '%',
+      secondaryValue: '5',
+      secondaryShiftValue: '%',
+      code: 'Digit5',
+      isPressed: false,
+    },
+    {
+      value: '6',
+      shiftValue: '^',
+      secondaryValue: '6',
+      secondaryShiftValue: '^',
+      code: 'Digit6',
+      isPressed: false,
+    },
+    {
+      value: '7',
+      shiftValue: '&',
+      secondaryValue: '7',
+      secondaryShiftValue: '?',
+      code: 'Digit7',
+      isPressed: false,
+    },
+    {
+      value: '8',
+      shiftValue: '*',
+      secondaryValue: '8',
+      secondaryShiftValue: '*',
+      code: 'Digit8',
+      isPressed: false,
+    },
+    {
+      value: '9',
+      shiftValue: '(',
+      secondaryValue: '9',
+      secondaryShiftValue: '(',
+      code: 'Digit9',
+      isPressed: false,
+    },
+    {
+      value: '0',
+      shiftValue: ')',
+      secondaryValue: '0',
+      secondaryShiftValue: ')',
+      code: 'Digit0',
+      isPressed: false,
+    },
+    {
+      value: '-',
+      shiftValue: '_',
+      secondaryValue: '-',
+      secondaryShiftValue: '_',
+      code: 'Minus',
+      isPressed: false,
+    },
+    {
+      value: '=',
+      shiftValue: '+',
+      secondaryValue: '=',
+      secondaryShiftValue: '+',
+      code: 'Equal',
+      isPressed: false,
+    },
+    {
+      value: 'BACKSPACE',
+      shiftValue: 'BACKSPACE',
+      secondaryValue: 'BACKSPACE',
+      secondaryShiftValue: 'BACKSPACE',
+      code: 'Backspace',
+      isPressed: false,
+    },
+    {
+      value: 'TAB',
+      shiftValue: 'TAB',
+      secondaryValue: 'TAB',
+      secondaryShiftValue: 'TAB',
+      code: 'Tab',
+      isPressed: false,
+    },
+    {
+      value: 'q',
+      shiftValue: 'Q',
+      secondaryValue: 'й',
+      secondaryShiftValue: 'Й',
+      code: 'KeyQ',
+      isPressed: false,
+    },
+    {
+      value: 'w',
+      shiftValue: 'W',
+      secondaryValue: 'ц',
+      secondaryShiftValue: 'Ц',
+      code: 'KeyW',
+      isPressed: false,
+    },
+    {
+      value: 'e',
+      shiftValue: 'E',
+      secondaryValue: 'у',
+      secondaryShiftValue: 'У',
+      code: 'KeyE',
+      isPressed: false,
+    },
+    {
+      value: 'r',
+      shiftValue: 'R',
+      secondaryValue: 'к',
+      secondaryShiftValue: 'К',
+      code: 'KeyR',
+      isPressed: false,
+    },
+    {
+      value: 't',
+      shiftValue: 'T',
+      secondaryValue: 'е',
+      secondaryShiftValue: 'Е',
+      code: 'KeyT',
+      isPressed: false,
+    },
+    {
+      value: 'y',
+      shiftValue: 'Y',
+      secondaryValue: 'н',
+      secondaryShiftValue: 'Н',
+      code: 'KeyY',
+      isPressed: false,
+    },
+    {
+      value: 'u',
+      shiftValue: 'U',
+      secondaryValue: 'г',
+      secondaryShiftValue: 'Г',
+      code: 'KeyU',
+      isPressed: false,
+    },
+    {
+      value: 'i',
+      shiftValue: 'I',
+      secondaryValue: 'ш',
+      secondaryShiftValue: 'Ш',
+      code: 'KeyI',
+      isPressed: false,
+    },
+    {
+      value: 'o',
+      shiftValue: 'O',
+      secondaryValue: 'щ',
+      secondaryShiftValue: 'Щ',
+      code: 'KeyO',
+      isPressed: false,
+    },
+    {
+      value: 'p',
+      shiftValue: 'P',
+      secondaryValue: 'з',
+      secondaryShiftValue: 'З',
+      code: 'KeyP',
+      isPressed: false,
+    },
+    {
+      value: '[',
+      shiftValue: '{',
+      secondaryValue: 'х',
+      secondaryShiftValue: 'Х',
+      code: 'BracketLeft',
+      isPressed: false,
+    },
+    {
+      value: ']',
+      shiftValue: '}',
+      secondaryValue: 'ъ',
+      secondaryShiftValue: 'Ъ',
+      code: 'BracketRight',
+      isPressed: false,
+    },
+    {
+      value: 'DEL',
+      shiftValue: 'DEL',
+      secondaryValue: 'DEL',
+      secondaryShiftValue: 'DEL',
+      code: 'Delete',
+      isPressed: false,
+    },
+    {
+      value: 'CAPSLOCK',
+      shiftValue: 'CAPSLOCK',
+      secondaryValue: 'CAPSLOCK',
+      secondaryShiftValue: 'CAPSLOCK',
+      code: 'CapsLock',
+      isPressed: false,
+    },
+    {
+      value: 'a',
+      shiftValue: 'A',
+      secondaryValue: 'ф',
+      secondaryShiftValue: 'Ф',
+      code: 'KeyA',
+      isPressed: false,
+    },
+    {
+      value: 's',
+      shiftValue: 'S',
+      secondaryValue: 'ы',
+      secondaryShiftValue: 'Ы',
+      code: 'KeyS',
+      isPressed: false,
+    },
+    {
+      value: 'd',
+      shiftValue: 'D',
+      secondaryValue: 'в',
+      secondaryShiftValue: 'В',
+      code: 'KeyD',
+      isPressed: false,
+    },
+    {
+      value: 'f',
+      shiftValue: 'F',
+      secondaryValue: 'а',
+      secondaryShiftValue: 'А',
+      code: 'KeyF',
+      isPressed: false,
+    },
+    {
+      value: 'g',
+      shiftValue: 'G',
+      secondaryValue: 'п',
+      secondaryShiftValue: 'П',
+      code: 'KeyG',
+      isPressed: false,
+    },
+    {
+      value: 'h',
+      shiftValue: 'H',
+      secondaryValue: 'р',
+      secondaryShiftValue: 'Р',
+      code: 'KeyH',
+      isPressed: false,
+    },
+    {
+      value: 'j',
+      shiftValue: 'J',
+      secondaryValue: 'о',
+      secondaryShiftValue: 'О',
+      code: 'KeyJ',
+      isPressed: false,
+    },
+    {
+      value: 'k',
+      shiftValue: 'K',
+      secondaryValue: 'л',
+      secondaryShiftValue: 'Л',
+      code: 'KeyK',
+      isPressed: false,
+    },
+    {
+      value: 'l',
+      shiftValue: 'L',
+      secondaryValue: 'д',
+      secondaryShiftValue: 'Д',
+      code: 'KeyL',
+      isPressed: false,
+    },
+    {
+      value: ';',
+      shiftValue: ':',
+      secondaryValue: 'ж',
+      secondaryShiftValue: 'Ж',
+      code: 'Semicolon',
+      isPressed: false,
+    },
+    {
+      value: "'",
+      shiftValue: '"',
+      secondaryValue: 'э',
+      secondaryShiftValue: 'Э',
+      code: 'Quote',
+      isPressed: false,
+    },
+    {
+      value: 'ENTER',
+      shiftValue: 'ENTER',
+      secondaryValue: 'ENTER',
+      secondaryShiftValue: 'ENTER',
+      code: 'Enter',
+      isPressed: false,
+    },
+    {
+      value: 'SHIFT',
+      shiftValue: 'SHIFT',
+      secondaryValue: 'SHIFT',
+      secondaryShiftValue: 'SHIFT',
+      code: 'ShiftLeft',
+      isPressed: false,
+    },
+    {
+      value: '\\',
+      shiftValue: '|',
+      secondaryValue: '\\',
+      secondaryShiftValue: '/',
+      code: 'IntlBackslash',
+      isPressed: false,
+    },
+    {
+      value: 'z',
+      shiftValue: 'Z',
+      secondaryValue: 'я',
+      secondaryShiftValue: 'Я',
+      code: 'KeyZ',
+      isPressed: false,
+    },
+    {
+      value: 'x',
+      shiftValue: 'X',
+      secondaryValue: 'ч',
+      secondaryShiftValue: 'Ч',
+      code: 'KeyX',
+      isPressed: false,
+    },
+    {
+      value: 'c',
+      shiftValue: 'C',
+      secondaryValue: 'с',
+      secondaryShiftValue: 'С',
+      code: 'KeyC',
+      isPressed: false,
+    },
+    {
+      value: 'v',
+      shiftValue: 'V',
+      secondaryValue: 'м',
+      secondaryShiftValue: 'М',
+      code: 'KeyV',
+      isPressed: false,
+    },
+    {
+      value: 'b',
+      shiftValue: 'B',
+      secondaryValue: 'и',
+      secondaryShiftValue: 'И',
+      code: 'KeyB',
+      isPressed: false,
+    },
+    {
+      value: 'n',
+      shiftValue: 'N',
+      secondaryValue: 'т',
+      secondaryShiftValue: 'Т',
+      code: 'KeyN',
+      isPressed: false,
+    },
+    {
+      value: 'm',
+      shiftValue: 'M',
+      secondaryValue: 'ь',
+      secondaryShiftValue: 'Ь',
+      code: 'KeyM',
+      isPressed: false,
+    },
+    {
+      value: ',',
+      shiftValue: '<',
+      secondaryValue: 'б',
+      secondaryShiftValue: 'Б',
+      code: 'Comma',
+      isPressed: false,
+    },
+    {
+      value: '.',
+      shiftValue: '>',
+      secondaryValue: 'ю',
+      secondaryShiftValue: 'Ю',
+      code: 'Period',
+      isPressed: false,
+    },
+    {
+      value: '/',
+      shiftValue: '?',
+      secondaryValue: '.',
+      secondaryShiftValue: ',',
+      code: 'Slash',
+      isPressed: false,
+    },
+    {
+      value: '↑',
+      shiftValue: '↑',
+      secondaryValue: '↑',
+      secondaryShiftValue: '↑',
+      code: 'ArrowUp',
+      isPressed: false,
+    },
+    {
+      value: 'SHIFT',
+      shiftValue: 'SHIFT',
+      secondaryValue: 'SHIFT',
+      secondaryShiftValue: 'SHIFT',
+      code: 'ShiftRight',
+      isPressed: false,
+    },
+    {
+      value: 'CTRL',
+      shiftValue: 'CTRL',
+      secondaryValue: 'CTRL',
+      secondaryShiftValue: 'CTRL',
+      code: 'ControlLeft',
+      isPressed: false,
+    },
+    {
+      value: 'WIN',
+      shiftValue: 'WIN',
+      secondaryValue: 'WIN',
+      secondaryShiftValue: 'WIN',
+      code: 'MetaLeft',
+      isPressed: false,
+    },
+    {
+      value: 'ALT',
+      shiftValue: 'ALT',
+      secondaryValue: 'ALT',
+      secondaryShiftValue: 'ALT',
+      code: 'AltLeft',
+      isPressed: false,
+    },
+    {
+      value: ' ',
+      shiftValue: ' ',
+      secondaryValue: ' ',
+      secondaryShiftValue: ' ',
+      code: 'Space',
+      isPressed: false,
+    },
+    {
+      value: 'ALT',
+      shiftValue: 'ALT',
+      secondaryValue: 'ALT',
+      secondaryShiftValue: 'ALT',
+      code: 'AltRight',
+      isPressed: false,
+    },
+    {
+      value: 'WIN',
+      shiftValue: 'WIN',
+      secondaryValue: 'WIN',
+      secondaryShiftValue: 'WIN',
+      code: 'MetaRight',
+      isPressed: false,
+    },
+    {
+      value: 'CTRL',
+      shiftValue: 'CTRL',
+      secondaryValue: 'CTRL',
+      secondaryShiftValue: 'CTRL',
+      code: 'ControlRight',
+      isPressed: false,
+    },
+    {
+      value: '←',
+      shiftValue: '←',
+      secondaryValue: '←',
+      secondaryShiftValue: '←',
+      code: 'ArrowLeft',
+      isPressed: false,
+    },
+    {
+      value: '↓',
+      shiftValue: '↓',
+      secondaryValue: '↓',
+      secondaryShiftValue: '↓',
+      code: 'ArrowDown',
+      isPressed: false,
+    },
+    {
+      value: '→',
+      shiftValue: '→',
+      secondaryValue: '→',
+      secondaryShiftValue: '→',
+      code: 'ArrowRight',
       isPressed: false,
     },
   ],
   renderKeyboard() {
     this.keySetup.forEach((el) => {
-      const key = new Key(this.shiftDown, el.value, el.shiftValue, el.isPressed).createKey();
+      let key;
+      if (this.primaryLanguage) {
+        key = new Key(this.shiftDown, el.value, el.shiftValue, el.isPressed).createKey();
+      } else {
+        key = new Key(this.shiftDown, el.secondaryValue, el.secondaryShiftValue, el.isPressed)
+          .createKey();
+      }
       key.addEventListener('click', (e) => {
         const text = e.target.innerText;
-        textArea.value += text;
+        if (text.length === 1) {
+          textArea.value += text;
+        }
       });
       keyBoardKeys.append(key);
       keyboard.append(keyBoardKeys);
@@ -53,6 +564,7 @@ document.addEventListener('keydown', (e) => {
     virtualKeyboard.rerenderKeyboard();
   }
 });
+
 document.addEventListener('keyup', (e) => {
   if (e.code.includes('Shift') && !e.repeat) {
     virtualKeyboard = { ...virtualKeyboard, shiftDown: !virtualKeyboard.shiftDown };
@@ -61,6 +573,7 @@ document.addEventListener('keyup', (e) => {
 });
 
 document.addEventListener('keydown', (e) => {
+  e.preventDefault();
   if (e.key.length === 1) {
     textArea.value += e.key;
   }
@@ -70,9 +583,11 @@ document.addEventListener('keydown', (e) => {
     }
     return key;
   });
-  console.log(e);
-  virtualKeyboard.rerenderKeyboard();
+  if (!e.repeat) {
+    virtualKeyboard.rerenderKeyboard();
+  }
 });
+
 document.addEventListener('keyup', (e) => {
   virtualKeyboard.keySetup = virtualKeyboard.keySetup.map((key) => {
     if (key.code === e.code) {
@@ -81,4 +596,11 @@ document.addEventListener('keyup', (e) => {
     return key;
   });
   virtualKeyboard.rerenderKeyboard();
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.altKey && e.ctrlKey) {
+    virtualKeyboard.primaryLanguage = !virtualKeyboard.primaryLanguage;
+    virtualKeyboard.rerenderKeyboard();
+  }
 });
